2e1bf1ab016939a8f598a480495f3cc022fbad64
blame.me <blame.me@b5b477c3-3bf9-2e94-0d67-1e01b036c894>
2011-03-16 00:37:38 +0000

    doc3

--- MTA.JAVA/src/monopoly/Monopoly.java
+++ MTA.JAVA/src/monopoly/Monopoly.java
@@ -12,6 +12,8 @@ import assets.Asset;
 import cards.ShaffledDeck;
 
 /**
+ * public class Monopoly
+ * this is the monopoly game
  * @author Omer Shenhar & Shachar Butnaro
  *
  */
@@ -32,6 +34,10 @@ public class Monopoly
 	private ArrayList<Square> gameBoard;
 
 
+	/**
+	 * method private void init
+	 * this method initializes the game data members
+	 */
 	private void init()
 	{
 		MonopolyInitilizer gameInitializer = new Init();
@@ -46,6 +52,11 @@ public class Monopoly
 		gameBoard = gameInitializer.initBoard();
 	}
 
+	/**
+	 * method public void play()
+	 * this methods handles the game
+	 * 
+	 */
 	public void play()
 	{
 		int roundNumber = 1;
@@ -57,7 +68,12 @@ public class Monopoly
 		userInterface.notifyGameWinner(gamePlayers.get(0));
 	}
 
-	public void doRound(int roundNumber)
+	/**
+	 * method private void doRound(int roundNumber)
+	 * this method handles a single round of monopoly
+	 * @param roundNumber - the round number
+	 */
+	private void doRound(int roundNumber)
 	{
 		int currDieSum;
 		for (int playerIndex=0; playerIndex<gamePlayers.size() ; playerIndex++)
@@ -83,6 +99,11 @@ public class Monopoly
 		}
 	}
 
+	/**
+	 * method private int rollDie()
+	 * this method rool all the dice
+	 * @return the sum of the dice roll
+	 */
 	private int rollDie()
 	{
 		int result=0;
@@ -94,6 +115,11 @@ public class Monopoly
 		return result;
 	}
 
+	/**
+	 * method public boolean rollForADouble()
+	 * this is a method to be used by jail square . it rolls the die once
+	 * @return true if the dice are the same false otherwise
+	 */
 	public boolean rollForADouble()
 	{
 		int firstRollOutcome = die[0].rollDice();
@@ -105,30 +131,48 @@ public class Monopoly
 		return true; //Will only get here if all rolls were the same
 	}	
 
+	/**
+	 * method private int getActualNumPlayers()
+	 * @return the actual number of players at the time of the request
+	 */
 	private int getActualNumPlayers()
 	{
 		return getGamePlayers().size();
 	}
 
-	private void movePlayer(Player p, int currDieSum, boolean getBonus) {
-		int playerPos = p.getCurrentPosition();
+	/**
+	 * Method private void movePlayer(Player player, int currDieSum, boolean getBonus)
+	 * this method moves a player on the game board 
+	 * @param player - a non null player who is in the game to be moved
+	 * @param currDieSum - a number from 2 to 12 - how many steps should the player take
+	 * @param getBonus - true if the player should get a bonus if he goes thru start. false otherwise
+	 */
+	private void movePlayer(Player player, int currDieSum, boolean getBonus) {
+		int playerPos = player.getCurrentPosition();
 		playerPos+=currDieSum;//currDieSum;
 		if (playerPos>=GameManager.NUMBER_OF_SQUARES)
 		{
 			if (getBonus)
 			{
 				userInterface.notifyPassStartSquare(GameManager.START_PASS_BONUS);
-				p.ChangeBalance(GameManager.START_PASS_BONUS, GameManager.ADD);
+				player.ChangeBalance(GameManager.START_PASS_BONUS, GameManager.ADD);
 			}
 			playerPos=playerPos % GameManager.NUMBER_OF_SQUARES;
 		}
-		p.setCurrentPosition(playerPos);
-		userInterface.notifyPlayerLanded(p, gameBoard.get(p.getCurrentPosition()));
-		gameBoard.get(playerPos).playerArrived(p);
+		player.setCurrentPosition(playerPos);
+		userInterface.notifyPlayerLanded(player, gameBoard.get(player.getCurrentPosition()));
+		gameBoard.get(playerPos).playerArrived(player);
 	}
 
-	public void gotoNextSquareOfType(Player p, Class<? extends Square> type, boolean getBonus) {
-		int currpos=(p.getCurrentPosition()+1)%GameManager.NUMBER_OF_SQUARES;
+	/**
+	 * method public void gotoNextSquareOfType(Player player, Class<? extends Square> type, boolean getBonus)
+	 * this method moves player to next square of type type;
+	 * @param player - a non null active player to be moved
+	 * @param type - a class which extends Square
+	 * @param getBonus -  true if the player should get a bonus if he goes thru start. false otherwise
+	 */
+	public void gotoNextSquareOfType(Player player, Class<? extends Square> type, boolean getBonus) {
+		int currpos=(player.getCurrentPosition()+1)%GameManager.NUMBER_OF_SQUARES;
 		int numOfSteps = 1;
 		while(!gameBoard.get(currpos).getClass().getSimpleName().equals(type.getSimpleName()))
 		{
@@ -136,34 +180,46 @@ public class Monopoly
 			numOfSteps++;
 		}
 
-		movePlayer(p, numOfSteps, getBonus);
+		movePlayer(player, numOfSteps, getBonus);
 	}
 
-	public void removePlayerFromGame(Player p)
+	/**
+	 * method public void removePlayerFromGame(Player player)
+	 * this method safely removes a player from game (returns all his assets to treasury ,demolish houses and so on)
+	 * @param player a non null active player to be removed
+	 */
+	public void removePlayerFromGame(Player player)
 	{
-		ArrayList<Asset> assetList=p.getAssetList();
+		ArrayList<Asset> assetList=player.getAssetList();
 		while (!assetList.isEmpty())//remove ownership from all remaining assets
 		{
 			assetList.get(0).setOwner(GameManager.assetKeeper);//set owner removes itself from the list
 		}
-		if (p.getGetOutOfJailFreeCardPlaceHolder()!=null)
-			surprise.add(p.getGetOutOfJailFreeCardPlaceHolder());
-		gamePlayers.remove(p);
+		if (player.getGetOutOfJailFreeCardPlaceHolder()!=null)
+			surprise.add(player.getGetOutOfJailFreeCardPlaceHolder());
+		gamePlayers.remove(player);
 	}
 
-	public Player getPlayer(int index) throws IndexOutOfBoundsException
-	{
-		return getGamePlayers().get(index);
-	}
-
+	/**
+	 * method public ArrayList<Player> getGamePlayers() 
+	 * @return a list of  all players in the game
+	 */
 	public ArrayList<Player> getGamePlayers() {
 		return gamePlayers;
 	}
 
+	/** 
+	 * method public ShaffledDeck getSurprise()
+	 * @return the surprise Deck
+	 */
 	public ShaffledDeck getSurprise() {
 		return surprise;
 	}
 
+	/** 
+	 * method public ShaffledDeck getCallUp()
+	 * @return the CallUp Deck
+	 */
 	public ShaffledDeck getCallUp() {
 		return callUp;
 	}
